33b8901d3aaf31116579f2f083ccdcda3b44406b
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2021-11-17 16:32:05 -0700

    Removing references to unused vars

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,8 +4,8 @@ import { useWeb3React } from '@web3-react/core'
 import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
-import useGetPriceData from 'hooks/useGetPriceData'
-import useGetLocalProfile from 'hooks/useGetLocalProfile'
+// import useGetPriceData from 'hooks/useGetPriceData'
+// import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import { connectorsByName } from 'connectors'
 import links from './config'
 
@@ -13,9 +13,9 @@ const Menu: React.FC = (props) => {
   const { account, activate, deactivate } = useWeb3React()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
-  const priceData = useGetPriceData()
-  const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
-  const profile = useGetLocalProfile()
+  // const priceData = useGetPriceData()
+  // const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  // const profile = useGetLocalProfile()
 
   return (
     <UikitMenu
